e5f2d8a70ec16311146b103cf3a9014de50f6f25
flyfly17 <erinflynn@mail.adelphi.edu>
2014-04-08 20:45:05 -0400

    changed forms

--- consultant/consultant/views.py
+++ consultant/consultant/views.py
@@ -1,46 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
+def save_school(request):
+    
 
-def home(request):
-    """The about us link"""
+# def home(request):
+#     """The about us link"""
     
-    majors = {"education":  ("Adelphi", "Molloy", "Queens"), "psychology": ()}
+#     majors = {"education":  ("Adelphi", "Molloy", "Queens"), "psychology": ()}
     
-    context = {}
+#     context = {}
     
-    if majors == "education":
-        context[key] = value
+#     if majors == "education":
+#         context[key] = value
         
-    return render(request, 'home.html')
+#     return render(request, 'home.html')
     
 
-def about_us(request):
-    """The about us link"""
+# def about_us(request):
+#     """The about us link"""
     
-    return render(request, 'about_us.html')
+#     return render(request, 'about_us.html')
     
 
-def quick_facts(request):
-    """The quick facts link"""
+# def quick_facts(request):
+#     """The quick facts link"""
     
-    return render(request, 'quick_facts.html')
-    
-    
-def search(request):
-    """The search link"""
-    
-    return render(request, 'search.html')
+#     return render(request, 'quick_facts.html')
     
     
-def contact_us(request):
-    """The contact us link"""
+# def search(request):
+#     """The search link"""
     
-    return render(request, 'contact_us.html')
+#     return render(request, 'search.html')
     
-def quiz(request):
-    """The quiz link"""
     
-    return render(request, 'quiz.html')
+# def contact_us(request):
+#     """The contact us link"""
+    
+#     return render(request, 'contact_us.html')
+    
+# def quiz(request):
+#     """The quiz link"""
+    
+#     return render(request, 'quiz.html')
     
     
