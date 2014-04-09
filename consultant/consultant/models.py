2ca3dda7cdc4092651ad06d51349a700c7583248
flyfly17 <erinflynn@mail.adelphi.edu>
2014-04-08 20:50:14 -0400

    added Myth model

--- consultant/consultant/models.py
+++ consultant/consultant/models.py
@@ -5,11 +5,18 @@ from django.forms import ModelForm
 class School(models.Model):
 
   name = models.CharField(max_length=500)
-  four_year_college = models.Boolean(default=true)
-  is_public = models.Boolean(default=true)
+  four_year_college = models.Boolean(default=True)
+  is_public = models.Boolean(default=True)
   population = models.IntergerField()
   gender = models.CharField(max_length=2)
   location = models.IntergerField(default=0)
   zip_code = models.IntergerField(max_length=5)
   state = models.CharField(max_length=2)
   campus_style = models.IntergerField()
+
+class Myth(models.Model):
+
+	myth = models.TextField()
+	is_true = models.Boolean()
+	correct_count = models.IntergerField()
+	wrong_count = models.IntergerField()
