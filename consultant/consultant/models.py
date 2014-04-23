3eca5b56ae03f4fdda964c5cf986afecd7211ffb
kthompson08 <kimberlythompson2@mail.adelphi.edu>
2014-04-22 21:58:46 -0400

    more changes to models, admin survey, views, database

--- consultant/consultant/models.py
+++ consultant/consultant/models.py
@@ -5,12 +5,12 @@ from django.forms import ModelForm
 class School(models.Model):
 
   name = models.CharField(max_length=500)
-  four_year_college = models.Boolean(default=True)
-  # is_public = models.Boolean(default=True)
+  is_four_year = models.BooleanField(default=True)
+  # is_public = models.BooleanField(default=True)
   # population = models.IntergerField()
   # gender = models.CharField(max_length=6)
   # location = models.IntergerField(default=0)
-  # resident = models.Boolean(default=True)
+  # resident = models.BooleanField(default=True)
   # zip_code = models.IntergerField(max_length=5)
   # state = models.CharField(max_length=2)
 
--- consultant/consultant/views.py
+++ consultant/consultant/views.py
@@ -3,33 +3,37 @@ from django.http import HttpResponse, HttpResponseRedirect
 from consultant.models import *
 
     
-# def home(request):
-    # """The about us link"""
-    
-    # majors = {"education":  ("Adelphi", "Molloy", "Queens"), "psychology": ()}
-    
-    # context = {}
-    
-    # if majors == "education":
-        # context[key] = value
+def home(request):
+    """The about us link"""
         
-    # return render(request, 'home.html')
+    return render(request, 'home.html')
 
 def college_form(request):
     """Enter a new college"""
+    schools = School.objects.all()
+    context = {"schools": schools}
     
-    return render(request, 'school_survey_admin.html')
+    return render(request, 'school_survey_admin.html', context)
     
-# def college_search(request):
-    # """The search link"""
+def college_search(request):
+    """The search link"""
     
-    # return render(request, 'school_survey.html')
+    return render(request, 'school_survey.html')
     
-# def contact_us(request):
-    # """The contact us link"""
+def contact_us(request):
+    """The contact us link"""
     
-    # return render(request, 'contact_us.html')
+    return render(request, 'contact_us.html')
     
+def common_myths(request):
+    """The myths link"""
+    
+    return render(request, 'common_myths.html')
+
+def about_us(request):
+    """The about us link"""
+    
+    return render(request, 'about_us.html')
     
 def save_school(request):
 
@@ -39,6 +43,6 @@ def save_school(request):
 
     # send them to the home page after saving
     # this will call views.home() from above
-    return HttpResponseRedirect("/")
+    return HttpResponseRedirect("/new_college")
 
 
